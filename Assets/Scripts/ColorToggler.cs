edfb87199bc9702f67dc390ca3df389ff51c77d6
Joost van Schaik <joostvanschaik@outlook.com>
2022-06-08 20:39:19 +0200

    Initial code checkin

---
+++ Assets/Scripts/ColorToggler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ColorToggler : MRTKBaseInteractable
+{
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+}
